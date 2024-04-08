cc251a9a4120b3da04357b7189d7742248cc2914
Deepan Kumar <deepanrvdvenu@gmail.com>
2024-04-09 02:22:40 +0530

    files

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,12 +1,14 @@
 import { Express } from "express";
 // import healthCheck from "./v1/healthCheck.route";
 import userRoutes from "./v1/user.routes";
+import billRoutes from "./v1/bill.routes";
 
 
 const initializeRoutes = (app: Express) => {
     console.log("inside route")
     // Routes
     app.use("/v1/user",userRoutes);    
+    app.use("/v1/bill",billRoutes);
 };
 
 
