9c0083a67c79e0149224d98ad1f74ced1f43c8d2
Deepan Kumar <deepanrvdvenu@gmail.com>
2024-04-08 18:41:00 +0530

    login modified

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,5 +1,5 @@
 import { error } from "console";
-import {UserSession,User,chart_slacc } from "../models/index";
+import { UserSession, User, chart_slacc } from "../models/index";
 import bcrypt from "bcrypt";
 import { generateAccessToken } from "../utils/generateToken";
 import { ResponseStatus } from "../responseCode/code";
@@ -14,10 +14,38 @@ export default class UserService {
   ) => Promise<
     { user: User; error?: undefined } | { error: any; user?: undefined }
   >;
-  
+
   static forgetPasswordService: (req: any) => Promise<any>;
   static resetPasswordService: (req: any) => Promise<any>;
-  static login: (userName: string, password: string) => Promise<{ success: boolean; status: number; message: string; error?: undefined; user?: undefined; accessToken?: undefined; } | { error: string; success?: undefined; status?: undefined; message?: undefined; user?: undefined; accessToken?: undefined; } | { user: any; accessToken: string; success?: undefined; status?: undefined; message?: undefined; error?: undefined; }>;
+  static login: (
+    userName: string,
+    password: string
+  ) => Promise<
+    | {
+        success: boolean;
+        status: number;
+        message: string;
+        error?: undefined;
+        user?: undefined;
+        accessToken?: undefined;
+      }
+    | {
+        error: string;
+        success?: undefined;
+        status?: undefined;
+        message?: undefined;
+        user?: undefined;
+        accessToken?: undefined;
+      }
+    | {
+        user: any;
+        accessToken: string;
+        success?: undefined;
+        status?: undefined;
+        message?: undefined;
+        error?: undefined;
+      }
+  >;
 }
 
 UserService.signup = async (
@@ -29,7 +57,9 @@ UserService.signup = async (
   try {
     console.log(userName, vendorName, email, password);
     console.log("inside try");
-    const existingUser = await (User as any).findOne({ where: { email: email } });
+    const existingUser = await (User as any).findOne({
+      where: { email: email },
+    });
     console.log("existinguser");
     if (existingUser) {
       return { error: "Email already exists" };
@@ -52,91 +82,101 @@ UserService.signup = async (
 
 UserService.login = async (userName: string, password: string) => {
   try {
-    const user = await (chart_slacc as any).findOne({ where: { USER: userName } });
-
+    const user = await (chart_slacc as any).findOne({
+      where: { USER: userName },
+    });
 
     if (!user) {
       return {
         success: false,
         status: ResponseStatus.HTTP_BAD_GATEWAY,
-        message:messages.notRegistered ,
-      };;
+        message: messages.notRegistered,
+      };
     }
 
-    if(password!=user.dataValues.PASSWORD){
+    if (password !== user.dataValues.PASSWORD) {
       return {
         success: false,
         status: ResponseStatus.HTTP_BAD_GATEWAY,
-        message:messages.invalidLoginDetails ,
+        message: messages.invalidLoginDetails,
       };
-
     }
 
-    // if (!passwordMatch) {
-    //   return { error: "Invalid password" };
-    // }
+    // Check if the user has an existing session
+    let session = await UserSession.findOne({
+      where: { user_name: user.dataValues.SUBL_NAME },
+    });
+
+    if (!session) {
+      session = await UserSession.create({
+        user_name: user.dataValues.SUBL_NAME,
+        sup_code: user.dataValues.SUBL_CODE,
+        noOftimesLoggedin: 1,
+      });
+    } else {
+      await session.increment("noOftimesLoggedin");
+    }
 
     const accessToken = generateAccessToken(user.dataValues.USER);
-   const session= await UserSession.create({user_name:user.dataValues.SUBL_NAME,sup_code:user.dataValues.SUBL_CODE})
 
-   return {
-    success: true,
-    status: ResponseStatus.HTTP_OK,
-    message:messages.linksendMessageEmail,
-    data:{user, accessToken}
-  };
-    // return { user, accessToken };
+    return {
+      success: true,
+      status: ResponseStatus.HTTP_OK,
+      message: messages.linksendMessageEmail,
+      data: { user, accessToken },
+    };
   } catch (error) {
     return { error: (error as Error).message };
   }
 };
 
-UserService.forgetPasswordService = async (req: any): Promise<any>=> {
+UserService.forgetPasswordService = async (req: any): Promise<any> => {
   try {
     const { email_id } = req.body;
 
-    const isUserExist = await (User as any).findOne({ where:{email:  email_id  }});
-    console.log(isUserExist)
+    const isUserExist = await (User as any).findOne({
+      where: { email: email_id },
+    });
+    console.log(isUserExist);
     if (!isUserExist) {
       return {
         success: false,
         status: ResponseStatus.HTTP_BAD_GATEWAY,
-        message:messages.notRegistered ,
+        message: messages.notRegistered,
       };
     }
 
-    const user_id :number= isUserExist.id; 
+    const user_id: number = isUserExist.id;
 
     await sendMail(email_id, user_id);
 
     return {
       success: true,
       status: ResponseStatus.HTTP_OK,
-      message:messages.linksendMessageEmail,
+      message: messages.linksendMessageEmail,
     };
   } catch (e) {
     console.log(e);
   }
-}
+};
 
-UserService.resetPasswordService =  async (req: any): Promise<any>=> {
+UserService.resetPasswordService = async (req: any): Promise<any> => {
   try {
+    const id = req?.id;
 
-    const id = req?.id
-
-    const{password}=req.body
-    const hashedPassword = await bcrypt.hash(password,10)
-const update_password = await (User as any).update(
-      { password: hashedPassword }, 
-      { where: { id: id } } 
+    const { password } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const update_password = await (User as any).update(
+      { password: hashedPassword },
+      { where: { id: id } }
     );
-      
+
     return {
       success: true,
       status: ResponseStatus.HTTP_OK,
-      message:messages.resetPassword,
+      message: messages.resetPassword,
     };
   } catch (e) {
     console.log(e);
   }
-}
+};
